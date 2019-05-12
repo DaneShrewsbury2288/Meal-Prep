6452144356d501d313dd720ba0e0e32c08dae5c4
ralexwong <46982508+ralexwong@users.noreply.github.com>
2019-05-11 20:43:33 -0700

    changed colors of content jumbotron. fixed some typos and ID names.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,45 +2,6 @@
 
 $(document).ready(function () {
 
-    // Your web app's Firebase configuration
-
-    var config = {
-        apiKey: "AIzaSyDKrVHt43u2axKy28_3Oy1y1mmC9yj1TPU",
-        authDomain: "meal-prep-b23a3.firebaseapp.com",
-        databaseURL: "https://meal-prep-b23a3.firebaseio.com",
-        projectId: "meal-prep-b23a3",
-        storageBucket: "meal-prep-b23a3.appspot.com",
-        messagingSenderId: "413133199350",
-        appId: "1:413133199350:web:7a9b0fdb10a8cabc"
-    };
-
-    // Initialize Firebase
-    firebase.initializeApp(config);
-
-    // Create a variable to reference the database.
-    var database = firebase.database();
-
-    // login
-    var logInForm = document.querySelector("#user");
-    logInForm.addEventListener("submit", function() {
-        event.preventDefault();
-
-        // get user info
-        var email = $("#user-login").val();
-        var password = $("#user-password").val();
-
-
-        console.log(email);
-
-        // sign up the user
-        database.createUserWithEmailAndPassword(email, password)
-
-
-
-    });
-
-
-    
 
     //On click of x button, call api for images and nutrient information
     $(document).on("click", "#calculate", function () {
